1a2a7b426c49054987f879f8d09c36808e66c9c0
George Waters <gwatersdev@gmail.com>
2022-01-26 15:15:29 -0500

    Remove nuitka-build for git

--- nuitka-build/monopoly/__main__.py
+++
@@ -1,9 +0,0 @@
-import monopoly
-# from multiprocessing import freeze_support
-# import sys
-
-# This file is only needed as an entry point for PyInstaller
-
-if __name__ == '__main__':
-    # freeze_support() # Needed for PyInstaller
-    monopoly.main()
